5c98b8829ac11a3053bf232b93badb3b96e057e3
imfarazsoomro <farazsoomro602@gmail.com>
2022-10-14 15:49:31 +0500

    set stage_id

--- BluStar_email_templates/models/crm_lead.py
+++ BluStar_email_templates/models/crm_lead.py
@@ -18,7 +18,9 @@ class CrmLead(models.Model):
     @api.onchange('stage_id')
     def _onchange_stage_id(self):
         if self.stage_id.name.lower() in ('appointment needed', 'scheduled'):
+            stage_id = self.stage_id.id
             external_mail = self.env.ref('BluStar_email_templates.appointment_email_externals')
             internal_mail = self.env.ref('BluStar_email_templates.appointment_email_internal')
             external_mail.send_mail(self._origin.id, force_send=True)
             internal_mail.send_mail(self._origin.id, force_send=True)
+            self.stage_id = stage_id
