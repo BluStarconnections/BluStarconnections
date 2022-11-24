e3d30b63e6fce2aa5550475bfda4d234b9570e63
imfarazsoomro <farazsoomro602@gmail.com>
2022-11-24 16:20:35 +0500

    import csv data final version

--- blustar_data_import/wizards/get_csv.py
+++ blustar_data_import/wizards/get_csv.py
@@ -5,7 +5,8 @@ import os
 
 import pandas as pd
 
-from odoo import fields, models
+from odoo import fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class WizardGetFile(models.TransientModel):
@@ -15,6 +16,11 @@ class WizardGetFile(models.TransientModel):
     model_choices = [('crm', 'CRM')]
     csv_file = fields.Binary('Upload CSV', required=True)
     model_name = fields.Selection(model_choices, 'Model Name')
+    stage_id = fields.Many2one('crm.stage', 'CRM Stage')
+    appt_status = fields.Selection([
+        ('Opened', 'Opened'), ('Yes', 'Yes'), ('Assigned', 'Assigned'), ('Pending', 'Pending'),
+        ('Reschedule', 'Reschedule'), ('Confirm', 'Confirm'), ('Cancel', 'Cancel'), ('Credit', 'Credit'),
+        ('Paid out', 'Paid out'), ('No', 'No')], string='Appt Status', store=True, default='Opened')
 
     # On click of import button on wizard
     # and import the csv file in that model
@@ -22,7 +28,7 @@ class WizardGetFile(models.TransientModel):
         file = base64.b64decode(self.csv_file)
         file_string = file.decode('unicode_escape')
         cur_dir = os.path.dirname(os.path.abspath(__file__))
-        file_name = os.path.join(cur_dir, "demofile2.csv")
+        file_name = os.path.join(cur_dir, "demofile.csv")
 
         with open(file_name, "w", encoding="utf-8") as f:
             f.write(file_string)
@@ -46,6 +52,20 @@ class WizardGetFile(models.TransientModel):
             return datetime.datetime.strptime(val, '%m/%d/%Y')
         except:
             return None
+
+    def get_scheduling_user(self):
+        try:
+            scheduling_user = self.env['res.users'].sudo().search(
+                [('login', '=', 'info@blustarconnections.com')], limit=1)
+            if scheduling_user:
+                return scheduling_user.id
+        except:
+            scheduling_user = self.env['res.users'].sudo().search(
+                [('name', '=', 'Former Scheduling Agent')], limit=1)
+            if scheduling_user:
+                return scheduling_user.id
+            else:
+                return None
 
     def import_blustar_crm_data(self, row):
         opportunity_name = row['Company'] if 'Company' in row and self.check_is_nan(row['Company']) is False else ''
@@ -78,9 +98,9 @@ class WizardGetFile(models.TransientModel):
             if caller_id:
                 user_id = caller_id[0].id
             else:
-                user_id = existing_rec[0].user_id.id if existing_rec else None
+                user_id = self.get_scheduling_user()
         else:
-            user_id = existing_rec[0].user_id.id if existing_rec else None
+            user_id = self.get_scheduling_user()
 
         state_code = row['State'] if 'State' in row and self.check_is_nan(row['State']) is False else None
         if state_code is not None:
@@ -88,7 +108,17 @@ class WizardGetFile(models.TransientModel):
             if state:
                 state_id = state[0].id
             else:
-                state_id = None
+                try:
+                    country_id = self.env['res.country'].sudo().search([('code', '=', 'US')])
+                    state_data_dict = {
+                        'name': state_code,
+                        'code': state_code,
+                        'country_id': country_id.id,
+                    }
+                    new_state = self.env['res.country.state'].sudo().create(state_data_dict)
+                    state_id = new_state.id if new_state else None
+                except:
+                    state_id = None
         else:
             state_id = None
 
@@ -97,6 +127,8 @@ class WizardGetFile(models.TransientModel):
 
         data_dict = {
             'name': opportunity_name,
+            'stage_id': self.stage_id.id,
+            'appt_status': self.appt_status,
             'contact_first_name': firstname,
             'contact_last_name': lastname,
             'primary_email': email,
